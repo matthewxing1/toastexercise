3cdee458800278ac910917223f876d0735d8b2e5
Matthew <matthew.xing@gmail.com>
2023-02-08 08:57:58 -0800

    created state at top level, created cb function to store form data in state temporarily on submission calls (this is pushed into the onMessage queue) , next step is to create a timeout to delete temp data after a period of time

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 
 import Header from './Header';
 import Content from './Content';
+import { onMessage } from './service/mockServer';
 
 function App() {
+  const [tempFormData, setTempFormData] = useState({});
+
+  function submitForm(form){
+    const storedTempData = tempFormData;
+    storedTempData[form.id] = form.data;
+    setTempFormData(storedTempData);
+  }
+
+  onMessage(submitForm);
   return (
     <>
       <Header />
