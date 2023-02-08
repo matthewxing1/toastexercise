e050d45a13d5d4790026472f3f7e29ee70163325
Matthew <matthew.xing@gmail.com>
2023-02-08 12:57:23 -0800

    added toast for errors that will display the status code along with the error message

---
+++ src/ToastErr.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import Snackbar from '@mui/material/Snackbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function ToastErr({error}) {
+  const [open, setOpen] = useState(true);
+
+  //close sidebar upon hitting X
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const action = (
+    <>
+      <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+    
+  )
+
+  return (
+    <Box sx={{flexGrow: 1}}>
+      <Snackbar open={open} 
+        message={`ERROR ${error.status}: ${error.message}`}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        action={action}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+      />
+    </Box>
+  );
+}
