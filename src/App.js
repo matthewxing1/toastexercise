5d77ae03e8b408e06622aec5b38f550bc54bb93f
Matthew <matthew.xing@gmail.com>
2023-02-08 11:40:20 -0800

    no functionality change, code cleanup and addition of pseudocode

--- src/App.js
+++ src/App.js
@@ -4,28 +4,29 @@ import Container from '@mui/material/Container';
 import Header from './Header';
 import Content from './Content';
 import { onMessage } from './service/mockServer';
+import Toast from './Toast';
 
 function App() {
-  const [tempFormData, setTempFormData] = useState({});
+  const [tempFormData, setTempFormData] = useState([]);
 
   function submitForm(form){
     //store data temporarily in state
-    const storedTempData = tempFormData;
-    storedTempData[form.id] = form.data;
-    setTempFormData(storedTempData);
-    //delete form after 6 seconds
+    setTempFormData([...tempFormData, form]);
+    // delete form after 6 seconds
     setTimeout(() => {
-      delete storedTempData[form.id];
-      setTempFormData(storedTempData);
+      setTempFormData(tempFormData.slice(1));
     }, 6000);
   }
-
+  
   onMessage(submitForm);
+
+  //map over data, creating a new Toast for each form submitted
   return (
     <>
       <Header />
       <Container>
         <Content />
+        {tempFormData.map((form) => <Toast key={form.id} email={form.data.email}/>)}
       </Container>
     </>
   );
