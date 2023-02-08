4a4e116b0007c447c082ca9d59a90fe1ef363168
Matthew <matthew.xing@gmail.com>
2023-02-08 12:17:55 -0800

    added buttons to toast with some styling, no functionality attached yet

--- src/Toast.js
+++ src/Toast.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Snackbar from '@mui/material/Snackbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function Toast({email, firstName, lastName}) {
   const [open, setOpen] = useState(true);
@@ -10,16 +13,33 @@ export default function Toast({email, firstName, lastName}) {
     setOpen(false);
   };
 
+  const handleLike = () => {
+  }
+
+  const action = (
+    <>
+      <Button color="primary" size="small" onClick={handleLike}>
+        LIKE
+      </Button>
+      <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+    
+  )
+
   return (
     <Box sx={{flexGrow: 1}}>
-      <Snackbar
-        open={open}
-        message={<div>
+      <Snackbar open={open} 
+          message={
+          <div>
             <Typography>{`${firstName} ${lastName}`}</Typography>
             <Typography>{email}</Typography>
           </div>}
         autoHideDuration={6000}
         onClose={handleClose}
+        action={action}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
       />
     </Box>
   );
