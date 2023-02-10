28df0bc1fe2ba1233bcd379535b140e9ab6c4b01
Matthew <matthew.xing@gmail.com>
2023-02-09 21:39:06 -0800

    minor cleanup

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,7 +6,6 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-
 import { createMockFormSubmission } from './service/mockServer';
 
 export default function Header() {
